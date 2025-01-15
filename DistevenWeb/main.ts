26e70889674f6b35ae30830e847151464a6a4413
SyedAli310 <alizaidi310@gmail.com>
2025-01-15 20:48:57 +0530

    added vercel insights

--- DistevenWeb/main.ts
+++ DistevenWeb/main.ts
@@ -1,6 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
  
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -12,4 +13,6 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
+// vercel method calls
+injectSpeedInsights({});
 inject();
